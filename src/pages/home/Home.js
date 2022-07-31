7118619659b077951dc5e89aa2ec65bbbfc8d99e
csmate79 <csicsekmate39@gmail.com>
2022-07-31 13:21:41 +0200

    home page

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -5,10 +5,18 @@ const Home = () => {
     return (
         <div className={`${classes.container} ${classes['flex-direction']}`} id='home'>
             <div className={classes.container__image}>
-                <h1>Home</h1>
+                <img className='home' src={require('../../assets/images/home_img.png')} alt="" />
             </div>
             <div className={classes.container__infos}>
-                <p>Asd</p>
+                <h2>Fanka Decor</h2>
+                <img className={classes.container__infos__logo} src={require('../../assets/images/fanka_decor_logo.png')} alt="" />
+                <div className={classes.container__infos__text}>
+                    <p>Asd</p>
+                </div>
+                <div className={classes.container__infos__social}>
+                    <img src={require('../../assets/images/facebook.png')} alt="" />
+                    <img src={require('../../assets/images/instagram.png')} alt="" />
+                </div>
             </div>
         </div>
     )
